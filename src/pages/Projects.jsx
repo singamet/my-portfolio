716bbff5227e93a6cc9a39f1af2deee2b6e15d32
singamet <ametojsingh007@gmail.com>
2024-12-17 23:43:30 +0530

    Food App final

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -16,6 +16,13 @@ export default function Projects() {
         "This simple notebook app, built using React and Firebase, allows users to sign in or sign up and create personalized sticky notes. Users can effortlessly create, edit, delete, color code, and search their notes, making it easy to organize and retrieve information. The app employs Firebase's robust authentication system and Firestore database, ensuring secure and efficient data management. Enjoy a seamless note-taking experience with intuitive features and a user-friendly interface.",
       link: "https://singametnotebook.netlify.app/",
     },
+    {
+      image: "../images/burger-fries-plate.jpg",
+      title: "Food App",
+      description:
+        "This Food Ordering App uses a React front end to provide a dynamic and responsive user interface. The back end is built with Node.jsand Express, ensuring efficient and scalable server-side operations. MongoDB is used to store all essential data, offering flexibility and performance. JWT (JSON Web Tokens) is utilized for secure user authentication. Important routes, such as cart, orders, and favorites, are protected and require proper authorization to access, ensuring data security and a personalized experience. This integration of cutting-edge technologies aims to deliver a reliable, secure, and user-friendly food ordering platform.",
+      link: "https://singametfoodapp.netlify.app/",
+    },
   ];
 
   return (
